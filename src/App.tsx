4238c1e3b4a2e438f517e499a87ac53f348ea737
Jcerovsky <89118515+Jcerovsky@users.noreply.github.com>
2023-09-06 18:13:48 +0930

    add menu component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,54 @@
 import PlayerInfo from "./components/PlayerInfo";
-
+import { useNavigate } from "react-router-dom";
 
 function App() {
-
+  const navigate = useNavigate();
   return (
-    <div className='bg-violet-600 h-screen p-10 flex flex-col gap-8'>
-        <section className='tracking-widest flex justify-between text-white font-bold'>
-            <button className=' w-4/12 bg-violet-700 rounded-3xl cursor-pointer hover:scale-105 hover:transition-transform delay-100	ease-in-out hover:shadow-inner'>MENU</button>
-            <img src="src/assets/images/logo.svg" alt="menu logo consisting of four dots" className='w-2/12'/>
-            <button className=' w-4/12 bg-violet-700 rounded-3xl cursor-pointer hover:scale-105 hover:transition-transform delay-100	ease-in-out hover:shadow-inner'>RESTART</button>
-        </section>
-        <section className='flex gap-5 justify-center'>
-            <PlayerInfo player={'PLAYER 1'}/>
-            <PlayerInfo player={'PLAYER 2'}/>
-        </section>
-        <section className='ml-auto mr-auto'>
-            <img src="src/assets/images/board-layer-white-small.svg" alt="empty connect four board"/>
-        </section>
+    <div className="bg-violet-600 h-screen p-10 flex flex-col gap-8">
+      <section className="tracking-widest flex justify-between text-white font-bold">
+        <button
+          className=" w-4/12 bg-violet-700 rounded-3xl cursor-pointer hover:scale-105
+        hover:transition-transform delay-100 ease-in-out hover:shadow-inner"
+          onClick={() => navigate("/menu")}
+        >
+          MENU
+        </button>
+        <img
+          src="src/assets/images/logo.svg"
+          alt="menu logo consisting of four dots"
+          className="w-2/12"
+        />
+        <button
+          className=" w-4/12 bg-violet-700 rounded-3xl cursor-pointer hover:scale-105
+        hover:transition-transform delay-100 ease-in-out hover:shadow-inner"
+        >
+          RESTART
+        </button>
+      </section>
+      <section className="flex gap-5 justify-center">
+        <PlayerInfo player={"PLAYER 1"} />
+        <PlayerInfo player={"PLAYER 2"} />
+      </section>
+      <section className="ml-auto mr-auto relative">
+        <img
+          src="src/assets/images/board-layer-white-small.svg"
+          alt="empty connect four board"
+          className="border-b-8 border-black rounded-3xl"
+        />
+        <div className="relative">
+          <img
+            src="src/assets/images/turn-background-red.svg"
+            alt="house shaped icon with a timer"
+            className="absolute -bottom-36 left-14"
+          />
+          <div className="absolute text-white font-bold right-20 top-5 text-center ">
+            <p>PLAYER 1'S TURN</p>
+            <p className="text-6xl">1</p>
+          </div>
+        </div>
+      </section>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/PlayerInfo.tsx
+++ src/components/PlayerInfo.tsx
@@ -5,7 +5,7 @@ interface Props {
 function PlayerInfo({player}: Props) {
 
     const playerImgSrc = player === 'PLAYER 1'? 'player-one.svg' : 'player-two.svg'
-    const playerIconLayout = player === 'PLAYER 1'? '-left-6' : '-right-6'
+    const playerIconLayout = player === 'PLAYER 1'? '-left-7' : '-right-7'
     return (
         <div className='w-5/12 font-bold'>
             <div className='relative border-2 border-b-8 rounded-3xl text-center border-black bg-white py-3 text-sm' >
@@ -13,6 +13,7 @@ function PlayerInfo({player}: Props) {
                 <p className=' text-3xl'>12</p>
                 <img src={`src/assets/images/${playerImgSrc}`} className={`absolute top-3 ${playerIconLayout}`}/>
             </div>
+
 
         </div>
     );
